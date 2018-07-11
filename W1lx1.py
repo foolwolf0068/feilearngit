0ddfa669229044d7497aff22b00b911761365b77
foolwolf0068 <foolwolf0068@gmail.com>
2018-07-11 23:17:26 +0800

    add a new python file

---
+++ W1lx1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 27 00:45:52 2017
+中国大学MOOC课程
+《Python 语言程序设计》
+第一周
+@author: foolwolf0068
+"""
+
+# lx1
+str1 = input("请输入一个人的名字：")
+str2 = input("请输入一个国家的名字：")
+print("世界这么大，{0}想去{1}看看。".format(str1, str2))
+print("世界这么大，%s想去%s看看"%(str1, str2))
